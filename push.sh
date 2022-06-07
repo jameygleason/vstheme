3a70929460e6183c6196155230a8ed6e368f5623
jameygleason <james.p.gleason@gmail.com>
2022-06-07 12:58:22 -0400

    initial

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+git remote set-url origin git@github.com:jameygleason/vstheme.git
+git push origin
+git remote set-url origin git@gitlab.com:jameygleason/vstheme.git
+git push origin
